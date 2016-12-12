c40df348212a301ef9b70baea6e601efeb944103
Terkal Robert <terkalma@gmail.com>
2016-12-12 15:25:57 +0100

    extracting user api related code to a concern

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,11 +1,1 @@
-Spree::User.class_eval do
-
-    before_validation :set_api_token
-
-    private
-
-    def set_api_token
-      self.spree_api_key ||= SecureRandom.hex(24)
-    end
-
-end
+Spree::User.send :include, UserHasApiToken
