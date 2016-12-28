ae493c8849d42d92d271149f26f64f13b8430113
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2016-12-28 20:49:22 +0200

    Change logic for verify_duplicate address

--- app/models/concerns/address_verify_duplicate.rb
+++ app/models/concerns/address_verify_duplicate.rb
@@ -5,9 +5,21 @@ module AddressVerifyDuplicate
     validate :verify_duplicate
   end
 
+  private
+
   def verify_duplicate
-    if user_id.present? && Spree::Address.where(attributes.except('id', 'updated_at', 'created_at')).any?
+    if user_id.present? && similar_resource_exists?
       errors.add(:base, 'Not a unique user address!')
     end
   end
+
+  def similar_resource_exists?
+    old_resources = Spree::Address.where(attributes.except('id', 'updated_at', 'created_at'))
+    old_resources.any? ? exists_one?(old_resources) : false
+  end
+
+  def exists_one?(old_resources)
+    old_resources.map {|resource| resource.id != id && resource.user_id.present? }.include?(true)
+  end
+
 end
