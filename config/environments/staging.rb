61c9b578bffd94546a438c71c9dbaf8501a5ef78
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-01-06 12:59:06 +0200

    Set mailer

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -56,6 +56,15 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "eComm_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              ENV['mail_host'],
+      port:                 587,
+      user_name:            ENV['mail_user'],
+      password:             ENV['mail_password'],
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
