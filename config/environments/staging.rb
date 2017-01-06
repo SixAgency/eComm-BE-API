e86378fbe2e7328cdfb5c3082c8ba42e4832ef32
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-01-06 13:07:19 +0200

    Add default from email

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "eComm_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_options = {from: 'no-reply@mlxusa.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              ENV['mail_host'],
