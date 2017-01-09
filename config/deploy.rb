6614e05d58172f23237467bc06881789f4b3ba95
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-01-09 16:13:11 +0200

    load:defaults has already been invoked and can no longer be modified

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require 'figaro'
 require 'byebug'
-require 'whenever/capistrano'
+# require 'whenever/capistrano'
 
 Figaro.application = Figaro::Application.new environment: 'development',
                                              path: "#{`pwd`.strip}/config/application.yml"
@@ -34,7 +34,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 set :nginx_log_path,  "/logs/#{fetch(:application)}_#{fetch(:stage)}"
 
-set :whenever_cron,   -> { "#{fetch(:application)}_#{fetch(:stage)}" }
+# set :whenever_cron,   -> { "#{fetch(:application)}_#{fetch(:stage)}" }
 
 ## Defaults:
 # set :scm,           :git
