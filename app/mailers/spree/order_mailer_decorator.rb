791cbba2bb9234fba43937a9e318442f8813de53
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-01-10 20:46:56 +0200

    Finish new order email

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,6 +1,8 @@
 Spree::OrderMailer.class_eval do
-  def create_email(order, mail_attributes)
+  def confirm_email(order, resend=false)
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-    @mail_attributes = mail_attributes
+    subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+    subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
+    mail(to: @order.email, from: from_address, subject: subject)
   end
 end
