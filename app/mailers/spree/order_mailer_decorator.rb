2e3b8e5e1b635832424d535f331e0d8eb3118677
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-01-12 20:56:48 +0200

    Refactor and add Cancel Email feature

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -7,4 +7,12 @@ Spree::OrderMailer.class_eval do
     target_email = @to_internal ? DefaultAppSettings::ORDER_EMAIL_TO : @order.email
     mail(to: target_email, from: from_address, subject: subject)
   end
+
+  def cancel_email(order, resend = false, to_internal=false)
+    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+    subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+    subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
+    target_email = to_internal ? DefaultAppSettings::ORDER_EMAIL_TO : @order.email
+    mail(to: target_email, from: from_address, subject: subject)
+  end
 end
--- app/models/concerns/order_email_delivery.rb
+++ app/models/concerns/order_email_delivery.rb
@@ -8,6 +8,11 @@ module OrderEmailDelivery
         Spree::OrderMailer.confirm_email(id, false, true).deliver_later
         update_column(:confirmation_delivered, true)
       end
+
+      def send_cancel_email
+        Spree::OrderMailer.cancel_email(id).deliver_later
+        Spree::OrderMailer.cancel_email(id, false, true).deliver_later
+      end
     end
   end
 
