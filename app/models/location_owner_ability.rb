39cc7dd13ba9b74620715238c28d91c31145df95
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-01-19 20:05:59 +0200

    Fix authorization on address RestApi. Delete default resources from users table when destroy the resource

--- app/models/location_owner_ability.rb
+++ app/models/location_owner_ability.rb
@@ -4,8 +4,22 @@ class LocationOwnerAbility
   def initialize(user)
     user ||= Spree::User.new
 
-    unless user.respond_to?(:has_spree_role?) && user.has_spree_role?('admin')
-      can [:read, :update, :destroy], Spree::Address, user_id: user.id
-    end
+
+  end
+end
+
+class LocationOwnerAbility
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= Spree::User.new
+
+    can [:read, :update, :destroy, :default_resources], Spree::Address, user_id: user.id
+
+
+    # if user.respond_to?(:has_spree_role?) && user.has_spree_role?('admin')
+    #   can [:display], Spree::Address, user_id: user.id
+    # end
+
   end
 end
