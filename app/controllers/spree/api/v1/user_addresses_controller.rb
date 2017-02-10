eed32ba73d8e1cfda2e2b03380b857ee12ee4c35
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-02-10 18:11:24 +0200

    Fix unique address validation.

--- app/controllers/spree/api/v1/user_addresses_controller.rb
+++ app/controllers/spree/api/v1/user_addresses_controller.rb
@@ -5,6 +5,7 @@ module Spree
         load_and_authorize_resource :class => Address, except: [:index, :create]
 
         before_action :resource, only: [:show, :update, :destroy]
+        before_action :set_current_user_to_address_model, only: [:create, :update]
 
         ADDRESS_TYPE = %w(bill ship)
 
@@ -142,6 +143,10 @@ module Spree
           current_api_user.save if must_save
         end
 
+        def set_current_user_to_address_model
+          # need for validation
+          Spree::Address::set_current_api_user(current_api_user)
+        end
       end
     end
   end
--- app/models/concerns/address_verify_duplicate.rb
+++ app/models/concerns/address_verify_duplicate.rb
@@ -2,10 +2,11 @@ module AddressVerifyDuplicate
   extend ActiveSupport::Concern
 
   included do
-    validate :verify_duplicate
+    validate :verify_duplicate, on: [:create, :update]
+    @@current_api_user = nil
 
     def self.set_current_api_user(user)
-      @current_api_user ||= user
+      @@current_api_user ||= user
     end
   end
 
@@ -21,6 +22,6 @@ module AddressVerifyDuplicate
   def similar_resource_exists?
     Spree::Address
         .where(attributes.except('id', 'updated_at', 'created_at'))
-        .where(user_id: @current_api_user).any?
+        .where(user_id: @@current_api_user).any?
   end
 end
