f259942d45032caa6d48990331e04f3957589210
Daniel Boldan <boldan.daniel@gmail.com>
2017-02-22 02:52:50 +0200

    Fix deploy rake command for swagger-docs

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,7 +77,7 @@ namespace :deploy do
   end
 
   after  :finishing,    :compile_assets
-  after  :finishing,    "swagger:docs"
+  # after  :finishing,    "swagger:docs"
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 
