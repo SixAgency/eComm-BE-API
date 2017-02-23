b84458d56dfe9eeaa4f28bb5b96e1ffcfd8f9d6c
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-02-23 17:22:18 +0200

    Unfix warning: class variable access from toplevel for ApiHelpers module.

--- app/controllers/concerns/payment_with_refunds.rb
+++ app/controllers/concerns/payment_with_refunds.rb
@@ -7,9 +7,7 @@ module PaymentWithRefunds
                         .result
                         .page(params[:page])
                         .per(params[:per_page])
-                        .includes(refunds: [
-                                               :id, :amount, :refund_reason_id
-                                           ])
+                        .includes(:refunds)
         respond_with(@payments)
       end
     end
--- app/helpers/spree/api/api_helpers_decorator.rb
+++ app/helpers/spree/api/api_helpers_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Api::ApiHelpers.module_eval do
-  mattr_reader :refund_attributes
-
-  class_variable_set(:@@refund_attributes, [ :id, :amount, :refund_reason_id])
+  # mattr_reader :refund_attributes
+  # class_variable_set(:@@refund_attributes, [ :id, :amount, :refund_reason_id])
+  Spree::Api::ApiHelpers::ATTRIBUTES << :refund_attributes
+  @@refund_attributes = [:id, :amount, :refund_reason_id]
 end
