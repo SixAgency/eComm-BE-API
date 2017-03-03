81bd0fa71f023532079bb8d09fb1aeecd8b62d63
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-03-03 20:50:58 +0200

    Current User change password

--- app/controllers/user_registrations_controller_decorator.rb
+++ app/controllers/user_registrations_controller_decorator.rb
@@ -46,4 +46,48 @@ Spree::UserRegistrationsController.class_eval do
     end
   end
 
+  # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+            :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+
+      sign_in resource_name, resource, bypass: true
+
+      respond_to do |format|
+        format.json {
+          render :json => {:message => "User was updated successfully"}
+        }
+
+        format.html {
+          respond_with resource, location: after_update_path_for(resource)
+        }
+      end
+
+    else
+      clean_up_passwords resource
+
+      respond_to do |format|
+        format.json {
+          render :json => {:errors => resource.errors.full_messages}
+        }
+
+        format.html {
+          respond_with resource
+        }
+      end
+
+    end
+  end
 end
