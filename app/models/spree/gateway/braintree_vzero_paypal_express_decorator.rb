26bc41b4bd5cb1b193c0dcc98c02dc31f7d3c489
Daniel Boldan <boldan.daniel@gmail.com>
2017-03-07 15:41:54 +0200

    Add refund, void features on cancel orders action

---
+++ app/models/spree/gateway/braintree_vzero_paypal_express_decorator.rb
@@ -0,0 +1,20 @@
+module Spree
+  Gateway::BraintreeVzeroPaypalExpress.class_eval do
+
+    # This method will implement refund feature on cancel order action.
+    def cancel(response_code)
+      provider
+      transaction = ::Braintree::Transaction.find(response_code)
+
+      ## From: https://www.braintreepayments.com/docs/ruby/transactions/refund
+      ## "A transaction can be refunded if its status is settled or settling.
+      ## If the transaction has not yet begun settlement, it should be voided instead of refunded.
+
+      if transaction.status == Braintree::Transaction::Status::SubmittedForSettlement
+        void(response_code)
+      else
+        credit(sprintf('%02.f', transaction.amount*100), response_code, {})
+      end
+    end
+  end
+end
