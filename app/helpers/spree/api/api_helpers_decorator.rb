9f37be008825d13247b3e5ea31e99251c137b624
Daniel Boldan <boldan.daniel@gmail.com>
2017-03-09 01:03:02 +0200

    Fix class variable access from toplevel

--- app/helpers/spree/api/api_helpers_decorator.rb
+++ app/helpers/spree/api/api_helpers_decorator.rb
@@ -1,6 +1,4 @@
 Spree::Api::ApiHelpers.module_eval do
-  # mattr_reader :refund_attributes
-  # class_variable_set(:@@refund_attributes, [ :id, :amount, :refund_reason_id])
-  Spree::Api::ApiHelpers::ATTRIBUTES << :refund_attributes
-  @@refund_attributes = [:id, :amount, :refund_reason_id]
+  class_variable_set(:@@refund_attributes, [ :id, :amount, :refund_reason_id])
+  mattr_reader :refund_attributes
 end
--- app/views/spree/api/v1/refunds/show.v1.rabl
+++ app/views/spree/api/v1/refunds/show.v1.rabl
@@ -1,3 +1,3 @@
 object @refund
 cache [I18n.locale, root_object]
-attributes *@@refund_attributes
+attributes *refund_attributes
