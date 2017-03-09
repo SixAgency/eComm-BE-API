fdee43ba5d4df480d34d73aa763ee92ed4545ef3
Daniel Boldan <boldan.daniel@gmail.com>
2017-03-10 01:41:45 +0200

    Gift Card Overrides

--- app/models/spree/store_credit_category_decorator.rb
+++
@@ -1,2 +0,0 @@
-Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME = "Gift Card".freeze
-Spree::StoreCreditCategory.non_expiring_credit_types |= [Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME]
--- app/overrides/admin_product_decorator.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/admin/products/new',
-                     :name => 'add_gift_card_to_product_edit',
-                     insert_after: '[data-hook="new_product_shipping_category"]',
-                     :partial => 'spree/admin/products/edit_gift_card')
