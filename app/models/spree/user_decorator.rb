56ca5a976db00c45696f4a43002a52a82fda08b9
Daniel Boldan <boldan.daniel@gmail.com>
2017-03-15 01:09:08 +0200

    Add mine store credit

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,4 +2,5 @@ Spree::User.send :include, UserHasApiToken, UserHasAddresses
 
 Spree::User.class_eval do
   has_one :square_customer, as: :owner
+  has_many :store_credit_events, through: :store_credits
 end
