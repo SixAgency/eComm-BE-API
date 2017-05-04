ffe492e41d1fc792af78e4264b47f58ff828a51d
Toth Tamas <tamas.toth@clever-software-solutions.com>
2017-05-04 20:17:40 +0300

    Limit the line_item quentity on merging two orders

---
+++ app/models/concerns/limit_line_item_quantity.rb
@@ -0,0 +1,9 @@
+module LimitLineItemQuantity
+
+    def handle_merge(current_line_item, other_order_line_item)
+        super
+        current_line_item.quantity = [current_line_item.quantity,
+                                      current_line_item.variant.max_quantity_allowed_in_cart].min
+        handle_error(current_line_item) unless current_line_item.save
+    end
+end
