28fc11bee94d5e1b5d840ce52e56cd93c25d3ce5
Toth Tamas <tamas.toth@clever-software-solutions.com>
2017-05-05 14:08:14 +0300

    remove_promo fix

--- app/controllers/concerns/remove_promotions.rb
+++ app/controllers/concerns/remove_promotions.rb
@@ -3,6 +3,7 @@ module RemovePromotions
   def remove_promotions
     authorize! :update, @order, order_token
     @order.adjustments.destroy_all
+    @order.promotions.clear
     @order.update_totals
     @order.persist_totals
     respond_with(@order, default_template: :show)
