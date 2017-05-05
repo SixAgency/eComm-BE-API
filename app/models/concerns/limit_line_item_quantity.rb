2ea5e502f07fd2ddf5a03a058afc8f64b6fd6ae1
Toth Tamas <tamas.toth@clever-software-solutions.com>
2017-05-05 14:41:10 +0300

    order_merger fix

--- app/models/concerns/limit_line_item_quantity.rb
+++ app/models/concerns/limit_line_item_quantity.rb
@@ -2,8 +2,10 @@ module LimitLineItemQuantity
 
     def handle_merge(current_line_item, other_order_line_item)
         super
-        current_line_item.quantity = [current_line_item.quantity,
-                                      current_line_item.variant.max_quantity_allowed_in_cart].min
-        handle_error(current_line_item) unless current_line_item.save
+        if current_line_item
+            current_line_item.quantity = [current_line_item.quantity,
+                                          current_line_item.variant.max_quantity_allowed_in_cart].min
+            handle_error(current_line_item) unless current_line_item.save
+        end
     end
 end
