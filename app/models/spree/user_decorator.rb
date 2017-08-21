aa416478c9ae32cd2cfb9f011c19cff3701b74cf
Sebestyen Andras <andras.sebestyen@clever-software-solutions.com>
2017-08-21 12:59:00 +0300

    gift card mail update + added full name to user model

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -3,4 +3,8 @@ Spree::User.send :include, UserHasApiToken, UserHasAddresses
 Spree::User.class_eval do
   has_one :square_customer, as: :owner
   has_many :store_credit_events, through: :store_credits
+
+  def full_name
+  	return [self.f_name, self.l_name].join(" ").strip
+  end
 end
